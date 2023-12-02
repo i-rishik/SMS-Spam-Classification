d756628a9eab64b0138fe8a4c80579fd5bc38f83
i-rishik <152501724+i-rishik@users.noreply.github.com>
2023-12-02 01:17:49 +0000

    Little change

--- Hello.py
+++ Hello.py
@@ -9,34 +9,35 @@ from nltk.corpus import stopwords
 
 ps = PorterStemmer()
 
-def transform_data(text):
+def run():
+  def transform_data(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
     res = []
     for i in text:
         if i.isalnum():
-            res.append(i)
+          res.append(i)
     
     text.clear()
     
     for i in res:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            text.append(i)
+      if i not in stopwords.words('english') and i not in string.punctuation:
+        text.append(i)
             
     res.clear()
     
     for i in text:
-        res.append(ps.stem(i))
+      res.append(ps.stem(i))
     
     return ' '.join(res)
 
-cv = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+  cv = pickle.load(open('vectorizer.pkl','rb'))
+  model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email Spam Classifier")
-input_sms = st.text_area("Enter the message")
+  st.title("Email Spam Classifier")
+  input_sms = st.text_area("Enter the message")
 
-if st.button('Predict'):
+  if st.button('Predict'):
 
     # 1. preprocess
     transformed_sms = transform_data(input_sms)
@@ -49,3 +50,6 @@ if st.button('Predict'):
         st.header("Spam")
     else:
         st.header("Not Spam")
+
+if __name__ == "__main__":
+    run()
